f9302103076dd98324694ced3a49e6f3ad752e28
Ferry <allan.ferry@orange.fr>
2018-12-11 15:30:21 +0100

    sged

--- Personnage.php
+++ Personnage.php
@@ -1,81 +1,102 @@
 <?php
-class Personnage 
+class Personnage
 {
-    private $_id ;
-    private $_degats;
-    private $_nom;
-
-    const CEST_MOI = 1;
-    const PERSONNAGE_TUE = 2;
-    const PERSONNAGE_FRAPPE = 3;
-
-    public function __construct(array $donnees)
+  private $_degats,
+          $_id,
+          $_nom;
+  
+  const CEST_MOI = 1; // Constante renvoyée par la méthode `frapper` si on se frappe soi-même.
+  const PERSONNAGE_TUE = 2; // Constante renvoyée par la méthode `frapper` si on a tué le personnage en le frappant.
+  const PERSONNAGE_FRAPPE = 3; // Constante renvoyée par la méthode `frapper` si on a bien frappé le personnage.
+  
+  
+  public function __construct(array $donnees)
+  {
+    $this->hydrate($donnees);
+  }
+  
+  public function frapper(Personnage $perso)
+  {
+    if ($perso->id() == $this->_id)
     {
-        $this->hydrate($donnees);
-    }
-
-    public function frapper ( Personnage $perso ) {
-        if ($perso->id() == $this->_id){
-            return self::CEST_MOI;
-        }
-        // Avant tout ne pas se frapper soi-même.
-        // Si c'est le cas, on stopppe tout en renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
-        // On indique au personnage frappé qu'il doit recevoir des dégats.
-        return $perso->recevoirDegats();
-    }
-
-    public function hydrate(array $donnees)
-    {
-        foreach ($donnees as $key => $value)
-        {
-            $method = 'set'.ucfirst($key);
-
-            if (method_exists($this, $method))
-            {
-                $this->$method($value);
-            }
-        }
-    }
-
-    public function recevoirDegats() {
-        // on augmente de 5 les dégats
-        if ($this->_degats >=100)
-        {
-            return self::PERSONNAGE_TUE;
-        }
-        // Si on a 100 de dégats ou plus, la méthode renverra une valeur signifiant que le personnage à été tué.
-        return self::PERSONNAGE_FRAPPE;
-        // Sinon, elle renverra une valeur signifiant que le personnage a été bien frappé.
-    }
-
-    // GETTERS //
-
-    public function degats() {
-        return $this->_degats;
+      return self::CEST_MOI;
     }
     
-    public function id() {
-        return $this->_id;
-    }
-
-    public function nom() {
-        return $this->_nom;
-    }
-
-    public function setDegats($degats) {
-        $degats = (int) $degats;
-
-        if ($degats >=0 && $degats <= 100)
-        {
-            $this->_degats = $degats;
-        }
-    }
-
-    public function setId($id)
+    // On indique au personnage qu'il doit recevoir des dégâts.
+    // Puis on retourne la valeur renvoyée par la méthode : self::PERSONNAGE_TUE ou self::PERSONNAGE_FRAPPE
+    return $perso->recevoirDegats();
+  }
+  
+  public function hydrate(array $donnees)
+  {
+    foreach ($donnees as $key => $value)
     {
-        if (is_string($nom))
-        {
-            $this->_nom = $nom;
-        }
+      $method = 'set'.ucfirst($key);
+      
+      if (method_exists($this, $method))
+      {
+        $this->$method($value);
+      }
     }
+  }
+  
+  public function recevoirDegats()
+  {
+    $this->_degats += 5;
+    
+    // Si on a 100 de dégâts ou plus, on dit que le personnage a été tué.
+    if ($this->_degats >= 100)
+    {
+      return self::PERSONNAGE_TUE;
+    }
+    
+    // Sinon, on se contente de dire que le personnage a bien été frappé.
+    return self::PERSONNAGE_FRAPPE;
+  }
+  
+  
+  // GETTERS //
+  
+  public function degats()
+  {
+    return $this->_degats;
+  }
+  
+  public function id()
+  {
+    return $this->_id;
+  }
+  
+  public function nom()
+  {
+    return $this->_nom;
+  }
+  
+  public function setDegats($degats)
+  {
+    $degats = (int) $degats;
+    
+    if ($degats >= 0 && $degats <= 100)
+    {
+      $this->_degats = $degats;
+    }
+  }
+  
+  public function setId($id)
+  {
+    $id = (int) $id;
+    
+    if ($id > 0)
+    {
+      $this->_id = $id;
+    }
+  }
+  
+  public function setNom($nom)
+  {
+    if (is_string($nom))
+    {
+      $this->_nom = $nom;
+    }
+  }
 }
