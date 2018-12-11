a453175c101faee280fed85c6886cd48932db859
Ferry <allan.ferry@orange.fr>
2018-12-11 13:57:22 +0100

    rrhaaaaaa!

--- index.php
+++ index.php
@@ -1,50 +1,55 @@
 <?php
-// on enregistre notre autoload
+// On enregistre notre autoload.
 function chargerClasse($classname)
 {
-    require $classname.'.php';
+  require $classname.'.php';
 }
-
 spl_autoload_register('chargerClasse');
-
-$db = new PDO('mysql:host=localhost;dbname=combats', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête à échoué
-
-$manager = new PersonnageManager($db);
-
-if (isset($_POST['creer']) && isset($_POST['nom'])) // si on a voulu créer un personnage
+session_start(); // On appelle session_start() APRÈS avoir enregistré l'autoload.
+if (isset($_GET['deconnexion']))
 {
-    $perso = new Personnage(['nom' => $_POST['nom']]); // on crée un nouveau personnage
-
-    if (!$perso->nomValide())
-    {
-        $message = 'le nom choisi est invalide.';
-        unset($perso);
-    }
-    elseif($manager->exists($perso->nom()))
-    {
-        $message = 'le nom du personnage est pris.';
-        unset($perso);
-    }
-    else
-    {
-        $manager->add($perso);
-    }
+  session_destroy();
+  header('Location: .');
+  exit();
 }
-
-elseif (isset($_POST['utiliser']) && isset($_POST['nom'])) // Si on a voulu utiliser un personnage
+if (isset($_SESSION['perso'])) // Si la session perso existe, on restaure l'objet.
 {
-    if($manager->exists($_POST['nom'])) // Si celui-ci existe
-    {
-        $perso = $manager->get($_POST['nom']);
-    }
-    else
-    {
-        $message = 'Ce personnage n\'existe pas !'; // S'il n'existe pas, on affichera ce message
-    }
+  $perso = $_SESSION['perso'];
+}
+$db = new PDO('mysql:host=localhost;dbname=combats', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+$manager = new PersonnagesManager($db);
+if (isset($_POST['creer']) && isset($_POST['nom'])) // Si on a voulu créer un personnage.
+{
+  $perso = new Personnage(['nom' => $_POST['nom']]); // On crée un nouveau personnage.
+  
+  if (!$perso->nomValide())
+  {
+    $message = 'Le nom choisi est invalide.';
+    unset($perso);
+  }
+  elseif ($manager->exists($perso->nom()))
+  {
+    $message = 'Le nom du personnage est déjà pris.';
+    unset($perso);
+  }
+  else
+  {
+    $manager->add($perso);
+  }
+}
+elseif (isset($_POST['utiliser']) && isset($_POST['nom'])) // Si on a voulu utiliser un personnage.
+{
+  if ($manager->exists($_POST['nom'])) // Si celui-ci existe.
+  {
+    $perso = $manager->get($_POST['nom']);
+  }
+  else
+  {
+    $message = 'Ce personnage n\'existe pas !'; // S'il n'existe pas, on affichera ce message.
+  }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -62,6 +67,8 @@ if (isset($message)) // On a un message à afficher ?
 if (isset($perso)) // Si on utilise un personnage (nouveau ou pas).
 {
 ?>
+    <p><a href="?deconnexion=1">Déconnexion</a></p>
+    
     <fieldset>
       <legend>Mes informations</legend>
       <p>
@@ -104,3 +111,8 @@ else
 ?>
   </body>
 </html>
+<?php
+if (isset($perso)) // Si on a créé un personnage, on le stocke dans une variable session afin d'économiser une requête SQL.
+{
+  $_SESSION['perso'] = $perso;
+}
