b0b11ffa081205d7683b6cace0096e6a7682baeb
Ferry <allan.ferry@orange.fr>
2018-12-11 09:43:57 +0100

    Initial commit

---
+++ Personnage.php
@@ -0,0 +1,17 @@
+<?php
+class Personnage {
+    private $_force ;
+    private $_experience;
+    private $_degats;
+
+    public function frapper() {
+
+    }
+
+    public function gagnerExperience() {
+
+    }
+
+    $perso = new Personnage;
+    $perso->_experience = $perso->_experience + 1;
+}
